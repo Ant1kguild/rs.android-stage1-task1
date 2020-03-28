75dda5a043a9fe32139c6fdaac662b35dc6be447
Andrey Mishchenko <ant1kguild@gmail.com>
2020-03-28 21:02:02 +0300

    init: first commit

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -1,9 +1,20 @@
 package subtask1
 
 class HappyArray {
-
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        fun swapArray(array: IntArray): IntArray {
+            val result = array.filterIndexed { index, value ->
+                if (index > 0 && index < array.size - 1)
+                    value < array[index - 1] + array[index + 1]
+                else
+                    true
+
+            }
+            return if (result.size == array.size)
+                result.toIntArray()
+            else
+                swapArray(result.toIntArray())
+        }
+        return swapArray(sadArray)
     }
 }
