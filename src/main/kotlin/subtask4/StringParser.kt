e50f2444c679931668cd850a8d4975074aa5dd98
Andrey Mishchenko <ant1kguild@gmail.com>
2020-03-28 21:04:10 +0300

    feat: add StringParser

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -1,9 +1,45 @@
 package subtask4
 
 class StringParser {
-
-    // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val temp = mutableListOf<IndexedValue<Char>>()
+        val bracketsOnly =
+            inputString.withIndex().filter { isOpen(it.value) || isClose(it.value) }
+        val openBrackets = bracketsOnly.filter { isOpen(it.value) }
+        val result = Array<String>(openBrackets.size) { _ -> "_" }
+
+        for (state in bracketsOnly) {
+            when {
+                isOpen(state.value) -> {
+                    temp.add(temp.size, state)
+                }
+                isClose(state.value) -> {
+                    val lastInd = temp.indexOfLast { it.value == open(state.value) }
+                    val start = temp.removeAt(lastInd)
+                    val startInt = start.index + 1
+                    val endInt = state.index - 1
+                    val index = openBrackets.indexOfFirst { it.index == start.index }
+                    result[index] = inputString.slice(startInt..endInt)
+                }
+            }
+        }
+        return result
+    }
+
+    private fun open(value: Char): Char {
+        return when (value) {
+            '>' -> '<'
+            ')' -> '('
+            ']' -> '['
+            else -> '_'
+        }
+    }
+
+    private fun isOpen(value: Char): Boolean {
+        return value == '<' || value == '(' || value == '['
+    }
+
+    private fun isClose(value: Char): Boolean {
+        return value == '>' || value == ')' || value == ']'
     }
 }
